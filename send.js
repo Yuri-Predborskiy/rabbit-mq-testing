53ea1f42c4b9a857b9e6444b32129b8e96e5c8d2
Yuri Predborski <shini1984@gmail.com>
2018-05-17 13:21:10 +0300

    Initialize repository. Add package.json. Create message sender. Source: https://www.rabbitmq.com/tutorials/tutorial-one-javascript.html

---
+++ send.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', (err, connection) => {
+    connection.createChannel(function(err, channel) {
+        let queue = 'hello';
+        let message = 'Hello World!';
+
+        channel.assertQueue(queue, {durable: false});
+        ch.sendToQueue(q, new Buffer(message));
+        console.log(` [x] Sent ${message}`);
+    });
+    setTimeout(function() { connection.close(); process.exit(0) }, 500);
+});
